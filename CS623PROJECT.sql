96c121cdf849c10923151133cdec1145393891c3
Tejaswini Paritala <108418494+tejaswiniparitala1415@users.noreply.github.com>
2022-12-22 10:07:10 +0530

    Add files via upload

---
+++ CS623PROJECT.sql
@@ -0,0 +1,21 @@
+﻿-- Table: public.stock
+
+-- DROP TABLE public.stock;
+
+CREATE TABLE public.stock
+(
+  prod character varying(50),
+  dep character varying(50),
+  quantity character varying(50),
+  CONSTRAINT stock_dep_fkey FOREIGN KEY (dep)
+      REFERENCES public.depot (dep) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT stock_prod_fkey FOREIGN KEY (prod)
+      REFERENCES public.product (prod) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.stock
+  OWNER TO postgres;
